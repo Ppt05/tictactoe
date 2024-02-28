8a25ee853f4bf1a5f1f5cf0048dfbe2b4cc29620
Ppt05 <160708311+Ppt05@users.noreply.github.com>
2024-02-28 15:36:36 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,13 @@ int main(){
     int n;
     cin >> n;
     for (int i=1;i<=n;i++){
-        for(int j=1;j<= n+1-i; j++){
-            cout << "*";
+        for(int j=1;j<= n-i; j++){
+            cout <<" ";
+
+        }
+
+        for(int j=1;j<=2*i-1;j++){
+            cout<< "*";
         }
 
         cout << endl;
