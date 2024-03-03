321520e231df7cad999161c1c6a41ffd2ee40ca4
Ppt05 <160708311+Ppt05@users.noreply.github.com>
2024-03-03 14:46:42 +0700

    Create struct.cpp

---
+++ struct.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    // nạp chồng toán tử
+    // kieu ratra e operator (= - ....) (danh sach tham số ) {//}
+    sophuc operator + (cónt sophuc other){
+    }
+
+      friend
+      ínertuion
+               hàm
+                      etwwe
+                      substring
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
